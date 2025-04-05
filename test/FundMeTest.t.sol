e23074c447c47ba253ff07d382cf3387389b9d69
Foursqweez <jeerasak.code@gmail.com>
2025-04-05 13:42:58 +0700

    Write testPriceFeedVersionIsAccurate test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -21,4 +21,19 @@ contract FunMeTest is Test {
         console.log("i_owner: ", fundMe.i_owner());
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    // What can we do to work with addresses outside our system?
+    // 1. Unit
+    //    - Testing a specific part of our code
+    // 2. Integration
+    //    - Testing how different parts of our code work together
+    // 3. Forked
+    //    - Testing our code on a simulated real environment
+    // 4. Staging
+    //    - Testing our code in a real environment that is not prod
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
 }
