88c6e458d95e0911ea66dc40db7c2af3ecbf210a
Foursqweez <jeerasak.code@gmail.com>
2025-04-05 12:40:30 +0700

    Debug Solidity tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,10 +8,17 @@ contract FunMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
+        // us -> FundMeTest -> FundMe
         fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        console.log("msg.sender: ", address(this));
+        console.log("i_owner: ", fundMe.i_owner());
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
