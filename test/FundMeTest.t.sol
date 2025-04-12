5fe67bc36b80d7f6ffab1bc6b72db28619c3b4f4
Foursqweez <jeerasak.code@gmail.com>
2025-04-12 13:52:55 +0700

    Optimize the withdraw function gas costs

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -126,4 +126,30 @@ contract FunMeTest is Test {
                 fundMe.getOwner().balance
         );
     }
+
+    function testWithDrawFromMultipleFundersCheaper() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal add ETH to address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
